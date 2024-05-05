d200ce5cc3c781636a33f66b1b344e26585d6026
Ambresh Vaishya <90375507+ambresh20@users.noreply.github.com>
2024-05-05 10:37:49 +0530

    Add files via upload

---
+++ src/Context/index.js
@@ -0,0 +1,8 @@
+
+import {useTodo, TodoProvider, TodoContext} from './TodoContext' ;
+
+export  {useTodo, TodoContext, TodoProvider} ;
+
+
+
+// index are only used for all file are same placed to imported . makes to easy importing files only 
